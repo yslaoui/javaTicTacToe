46980494c71e523a7f755199fe3c71c953c9dacd
yslaoui <yslaouiyassine@gmail.com>
2024-05-02 16:36:19 +0100

    created a 3 by 3 grid using an ArrayList of Button objects

--- Grid.java
+++ Grid.java
@@ -1,9 +1,14 @@
 package tictactoe;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Grid {
     public  Grid() {
@@ -11,15 +16,24 @@ public class Grid {
     }
     public Parent getGrid() {
 //        UI elements
-        Button button1 = new Button("X");
-        button1.setFont(Font.font("Monospaced", 40));
-        Button button2 = new Button("O");
-        button2.setFont(Font.font("Monospaced", 40));
+        List<Button> buttons = new ArrayList();
+        for (int i=0; i< 9; i++) {
+            Button button = new Button(" ");
+            button.setFont(Font.font("Monospaced", 40));
+            buttons.add(button);
+        }
 
 //        Layout
         GridPane grid = new GridPane();
-        grid.add(button1, 0, 0);
-        grid.add(button2, 1, 0);
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(20,20,20,20));
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                grid.add(buttons.get(3*i+j), i, j);
+            }
+        }
         return grid;
     }
 }
